c847b4dd38bf9a2eaf01d722af5ef2b93caac089
Demayne Collins <collinsd73@gmail.com>
2018-04-05 21:39:53 -0700

    completed first website

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,10 @@ class Blog(models.Model):
     body = models.TextField(max_length=200)
     pub_date = models.DateTimeField('date published')
     image = models.ImageField(upload_to='images/')
+
+
+    def __str__(self):
+         return self.title
+
+    def pub_date_revised(self):
+        return self.pub_date.strftime('%b %e %Y')
