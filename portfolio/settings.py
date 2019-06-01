2bbccca291d1ca1c3ada564403d8c8e06aa72fc0
Demayne Collins <33962430+collins73@users.noreply.github.com>
2019-06-01 14:38:05 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'shake1911',
+        'PASSWORD': 'xxxxxx',
         'HOST': 'localhost',
         'PORT': '5433',
     }
